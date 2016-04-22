20ba096c4d610ad9d5b8f244d4b514daf8db4c12
matmax <matthias.graf@st.ovgu.de>
2016-04-22 15:08:09 +0200

    filesize warning stat - bugfix

--- listFiles.php
+++ listFiles.php
@@ -5,8 +5,11 @@ $files = [];
 foreach (scandir($dir) as $id => $filename) {
 	// exclude directories and hidden files (like .htaccess and .keep)
 	if (!is_dir($dir.$filename) && $filename[0] !== '.') {
+		// may produce: Warning: filesize(): stat failed
+		// @ suppresses Warning
+		$size = @filesize($dir.$filename);
 		// append
-		$files[] = array('name' => $filename, 'size' => filesize($dir.$filename));
+		$files[] = array('name' => $filename, 'size' => ($size === false ? 0 : $size));
 	}
 }
 echo json_encode($files);
